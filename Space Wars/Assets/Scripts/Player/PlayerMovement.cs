bbc022a885405fbf4a962d6d456aa7ca3258d179
Maciej Pieczonka <m3r3zz@gmail.com>
2021-12-01 03:23:34 +0100

    Implemented moving, rotating, and clamping

---
+++ Space Wars/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Collider2D collider;
+    private Renderer renderer;
+
+    private Vector2 size;
+    private float xMax, yMax, xMin, yMin;
+    private float horizontal, vertical;
+    
+    private const float diagonalLimiter = 0.71f;
+    private const float rotationY = 45.0f;
+    
+    [SerializeField] private float velocity = 5.0f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        size = renderer.bounds.size;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontal = vertical = 0.0f;
+
+        // Get max/min possible positions
+        Vector2 screenSize = CameraManager.GetScreenSize();
+        xMax = screenSize.x / 2.0f - size.x / 2.0f;
+        yMax = screenSize.y / 2.0f - size.y / 2.0f;
+        xMin = -xMax;
+        yMin = -yMax;
+
+        // Check whether WASD keys are pressed
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) { horizontal = 0.0f; transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f); }
+        else if (Input.GetKey(KeyCode.A)) { horizontal = -1.0f; transform.rotation = Quaternion.Euler(0.0f, -rotationY, 0.0f); }
+        else if (Input.GetKey(KeyCode.D)) { horizontal = 1.0f; transform.rotation = Quaternion.Euler(0.0f, rotationY, 0.0f); } 
+        else transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        
+        if (Input.GetKey(KeyCode.S))
+            vertical -= 1.0f;
+        if (Input.GetKey(KeyCode.W))
+            vertical += 1.0f;
+
+        // Check whether player moves diagonally and if so, apply limiter
+        if (horizontal != 0.0f && vertical != 0.0f)
+        {
+            horizontal *= diagonalLimiter;
+            vertical *= diagonalLimiter;
+        }
+
+        // Check whether player reached max/min position and keeps moving in that direction
+        if (transform.position.x >= xMax && horizontal > 0.0f) horizontal = 0.0f;
+        else if (transform.position.x <= xMin && horizontal < 0.0f) horizontal = 0.0f;
+        if (transform.position.y >= yMax && vertical > 0.0f) vertical = 0.0f;
+        else if (transform.position.y <= yMin && vertical < 0.0f) vertical = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(horizontal * velocity, vertical * velocity);
+    }
+}
